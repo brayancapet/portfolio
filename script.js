4e6d636764e28077be75f03f3f71642e46395ada
brayan <brayan.capet@outlook.fr>
2022-04-18 23:14:17 +0200

    projets en cours

--- script.js
+++ script.js
@@ -9,12 +9,19 @@
 
 const presentation_right = document.querySelector(".presentation-right-div");
 const presentation_left = document.querySelector(".presentation-left-div");
+const projets_card = document.querySelectorAll(".card");
 
 window.addEventListener("scroll", () => {
   let scrollPos = window.scrollY;
   console.log(scrollPos);
-  if (scrollPos >= 580) {
+  if (scrollPos >= 580 && scrollPos <= 600) {
     presentation_right.classList.add("slide-in-from-right");
     presentation_left.classList.add("slide-in-from-left");
   }
+
+  // if (scrollPos >= 1000 && scrollPos <= 1020) {
+  //   projets_card.classList.add("projets-animation");
+  // }
 });
+
+console.log(projets_card);
