554e9bf72f4b53fcb78246255603f29708af0738
brayan <brayan.capet@outlook.fr>
2022-04-19 16:14:29 +0200

    version pc terminée

--- script.js
+++ script.js
@@ -10,6 +10,9 @@
 const presentation_right = document.querySelector(".presentation-right-div");
 const presentation_left = document.querySelector(".presentation-left-div");
 const projets_card = document.querySelectorAll(".card");
+const skill_left = document.querySelector(".skill-left");
+const skill_center = document.querySelector(".skill-center");
+const skill_right = document.querySelector(".skill-right");
 
 window.addEventListener("scroll", () => {
   let scrollPos = window.scrollY;
@@ -19,9 +22,17 @@ window.addEventListener("scroll", () => {
     presentation_left.classList.add("slide-in-from-left");
   }
 
-  // if (scrollPos >= 1000 && scrollPos <= 1020) {
-  //   projets_card.classList.add("projets-animation");
-  // }
+  if (scrollPos >= 1000 && scrollPos <= 1020) {
+    for (let i = 0; i < projets_card.length; i++) {
+      projets_card[i].classList.add("projets-animation");
+    }
+  }
+
+  if (scrollPos >= 1850 && scrollPos <= 1870) {
+    skill_left.classList.add("skill-effect-left");
+    skill_center.classList.add("skill-effect-center");
+    skill_right.classList.add("skill-effect-right");
+  }
 });
 
 console.log(projets_card);
